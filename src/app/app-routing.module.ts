b9a90ad51f530f6049dd198dc616094baafa3e08
joshnos <joshnos>
2017-09-19 14:02:42 -0400

    angular2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoComponent } from 'app/components/demo/demo.component';
+import { CompComponent } from 'app/components/comp/comp.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: 'demo', component: DemoComponent
+  },
+  {
+    path: 'comp', component: CompComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { CompComponent } from './components/comp/comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    DemoComponent,
+    CompComponent
   ],
   imports: [
     BrowserModule,
