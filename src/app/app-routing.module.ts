4dbc08725055a59314e2c01bcb2dfdfd5d96e64c
joshnos <joshnos>
2017-09-25 22:08:23 -0400

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { DemoComponent } from 'app/components/demo/demo.component';
 import { CompComponent } from 'app/components/comp/comp.component';
+import { Comp1Component } from 'app/components/comp1/comp1.component';
 
 const routes: Routes = [
   {
@@ -9,6 +11,9 @@ const routes: Routes = [
   },
   {
     path: 'comp', component: CompComponent
+  },
+  {
+    path: 'comp1', component: Comp1Component
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { CompComponent } from './components/comp/comp.component';
+import { Comp1Component } from './components/comp1/comp1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     DemoComponent,
-    CompComponent
+    CompComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule,
